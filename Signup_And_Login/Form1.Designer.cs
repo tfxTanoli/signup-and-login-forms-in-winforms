b0ffd403fe3cfde860fe9e5c940cb2b9613fdb24
tfxTanoli <usman5194999@gmail.com>
2025-11-19 20:42:34 +0500

    Add project files.

---
+++ Signup_And_Login/Form1.Designer.cs
@@ -0,0 +1,413 @@
+﻿namespace Signup_And_Login
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider4 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider5 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider6 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider7 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider8 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.errorProvider9 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider8)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider9)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.12155F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.87845F));
+			this.tableLayoutPanel1.Controls.Add(this.textBox9, 1, 8);
+			this.tableLayoutPanel1.Controls.Add(this.textBox8, 1, 7);
+			this.tableLayoutPanel1.Controls.Add(this.textBox7, 1, 6);
+			this.tableLayoutPanel1.Controls.Add(this.textBox6, 1, 5);
+			this.tableLayoutPanel1.Controls.Add(this.textBox5, 1, 4);
+			this.tableLayoutPanel1.Controls.Add(this.textBox4, 1, 3);
+			this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+			this.tableLayoutPanel1.Controls.Add(this.label6, 0, 4);
+			this.tableLayoutPanel1.Controls.Add(this.label7, 0, 5);
+			this.tableLayoutPanel1.Controls.Add(this.label8, 0, 6);
+			this.tableLayoutPanel1.Controls.Add(this.label9, 0, 7);
+			this.tableLayoutPanel1.Controls.Add(this.label10, 0, 8);
+			this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(212, 96);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 9;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(498, 528);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(320, 18);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(197, 36);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Signup Form";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(4, 0);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(94, 20);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Student Id";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(4, 58);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(127, 20);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "Student Name";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(4, 116);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(140, 20);
+			this.label4.TabIndex = 2;
+			this.label4.Text = "Student Gender";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(4, 174);
+			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(117, 20);
+			this.label5.TabIndex = 3;
+			this.label5.Text = "Father Name";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(4, 232);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(41, 20);
+			this.label6.TabIndex = 4;
+			this.label6.Text = "Age";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(4, 290);
+			this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(57, 20);
+			this.label7.TabIndex = 5;
+			this.label7.Text = "Class";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(4, 348);
+			this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(56, 20);
+			this.label8.TabIndex = 6;
+			this.label8.Text = "Email";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.Location = new System.Drawing.Point(4, 406);
+			this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(91, 20);
+			this.label9.TabIndex = 7;
+			this.label9.Text = "Password";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.Location = new System.Drawing.Point(4, 464);
+			this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(163, 20);
+			this.label10.TabIndex = 8;
+			this.label10.Text = "Confirm Password";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(404, 632);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(182, 48);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Signup";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(192, 3);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(194, 27);
+			this.textBox1.TabIndex = 10;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(192, 61);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(194, 27);
+			this.textBox2.TabIndex = 11;
+			this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+			this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(192, 119);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(194, 27);
+			this.textBox3.TabIndex = 12;
+			this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(192, 177);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(194, 27);
+			this.textBox4.TabIndex = 13;
+			this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+			this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(192, 235);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(194, 27);
+			this.textBox5.TabIndex = 14;
+			this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
+			this.textBox5.Leave += new System.EventHandler(this.textBox5_Leave);
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(192, 293);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(194, 27);
+			this.textBox6.TabIndex = 15;
+			this.textBox6.Leave += new System.EventHandler(this.textBox6_Leave);
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(192, 351);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(194, 27);
+			this.textBox7.TabIndex = 16;
+			this.textBox7.Leave += new System.EventHandler(this.textBox7_Leave);
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(192, 409);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(194, 27);
+			this.textBox8.TabIndex = 17;
+			this.textBox8.UseSystemPasswordChar = true;
+			this.textBox8.Leave += new System.EventHandler(this.textBox8_Leave);
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(192, 467);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.Size = new System.Drawing.Size(194, 27);
+			this.textBox9.TabIndex = 18;
+			this.textBox9.UseSystemPasswordChar = true;
+			this.textBox9.Leave += new System.EventHandler(this.textBox9_Leave);
+			// 
+			// errorProvider1
+			// 
+			this.errorProvider1.ContainerControl = this;
+			// 
+			// errorProvider2
+			// 
+			this.errorProvider2.ContainerControl = this;
+			// 
+			// errorProvider3
+			// 
+			this.errorProvider3.ContainerControl = this;
+			// 
+			// errorProvider4
+			// 
+			this.errorProvider4.ContainerControl = this;
+			// 
+			// errorProvider5
+			// 
+			this.errorProvider5.ContainerControl = this;
+			// 
+			// errorProvider6
+			// 
+			this.errorProvider6.ContainerControl = this;
+			// 
+			// errorProvider7
+			// 
+			this.errorProvider7.ContainerControl = this;
+			// 
+			// errorProvider8
+			// 
+			this.errorProvider8.ContainerControl = this;
+			// 
+			// errorProvider9
+			// 
+			this.errorProvider9.ContainerControl = this;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(980, 703);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "Signup";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider8)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider9)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ErrorProvider errorProvider1;
+		private System.Windows.Forms.ErrorProvider errorProvider2;
+		private System.Windows.Forms.ErrorProvider errorProvider3;
+		private System.Windows.Forms.ErrorProvider errorProvider4;
+		private System.Windows.Forms.ErrorProvider errorProvider5;
+		private System.Windows.Forms.ErrorProvider errorProvider6;
+		private System.Windows.Forms.ErrorProvider errorProvider7;
+		private System.Windows.Forms.ErrorProvider errorProvider8;
+		private System.Windows.Forms.ErrorProvider errorProvider9;
+	}
+}
+
---
+++ Signup_And_Login/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Signup_And_Login
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox1.Text) && string.IsNullOrEmpty(textBox2.Text) && string.IsNullOrEmpty(textBox3.Text) && string.IsNullOrEmpty(textBox4.Text) && string.IsNullOrEmpty(textBox5.Text) && string.IsNullOrEmpty(textBox6.Text) && string.IsNullOrEmpty(textBox7.Text) && string.IsNullOrEmpty(textBox8.Text) && string.IsNullOrEmpty(textBox9.Text))
+			{
+				MessageBox.Show("Please Fill All The Details");
+			}
+			else if (textBox8.Text != textBox9.Text)
+			{
+				MessageBox.Show("Password and Confirm Password do not match");
+			}
+			else
+			{
+				string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+				SqlConnection con = new SqlConnection(cs);
+				string query = "insert into signup values (@id, @name,@gender,@fathername,@age,@class,@email,@pass)";
+				SqlCommand cmd = new SqlCommand(query, con);
+				cmd.Parameters.AddWithValue("@id", textBox1.Text);
+				cmd.Parameters.AddWithValue("@name", textBox2.Text);
+				cmd.Parameters.AddWithValue("@gender", textBox3.Text);
+				cmd.Parameters.AddWithValue("@fathername", textBox4.Text);
+				cmd.Parameters.AddWithValue("@age", textBox5.Text);
+				cmd.Parameters.AddWithValue("@class", textBox6.Text);
+				cmd.Parameters.AddWithValue("@email", textBox7.Text);
+				cmd.Parameters.AddWithValue("@pass", textBox8.Text);
+
+				con.Open();
+				int a = cmd.ExecuteNonQuery();
+
+				if (a > 0)
+				{
+					MessageBox.Show("Data inserted successfully");
+				}
+				else
+				{
+					MessageBox.Show("Data not inserted");
+				}
+				con.Close();
+			}
+		}
+
+		private void textBox1_Leave(object sender, EventArgs e)
+		{
+			if(string.IsNullOrEmpty(textBox1.Text))
+			{
+				textBox1.Focus();
+				errorProvider1.SetError(this.textBox1, "Please Enter ID");
+			}
+			else
+			{
+				errorProvider1.Clear();
+			}
+		}
+
+		private void textBox2_Leave(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox2.Text))
+			{
+				textBox2.Focus();
+				errorProvider2.SetError(this.textBox2, "Please Enter Name");
+			}
+			else
+			{
+				errorProvider2.Clear();
+			}
+		}
+
+		private void textBox3_Leave(object sender, EventArgs e)
+		{
+			if(string.IsNullOrEmpty(textBox3.Text))
+			{
+				textBox3.Focus();
+				errorProvider3.SetError(this.textBox3, "Please Enter Valid Gender");
+			}
+			else
+			{
+				errorProvider3.Clear();
+			}
+		}
+
+		private void textBox4_Leave(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox4.Text))
+			{
+				textBox4.Focus();
+				errorProvider4.SetError(this.textBox4, "Please Enter Father Name");
+			}
+			else
+			{
+				errorProvider4.Clear();
+			}
+		}
+
+		private void textBox5_Leave(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox5.Text))
+			{
+				textBox5.Focus();
+				errorProvider5.SetError(this.textBox5, "Please Enter Age");
+			}
+			else
+			{
+				errorProvider5.Clear();
+			}
+		}
+
+		private void textBox6_Leave(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox6.Text))
+			{
+				textBox6.Focus();
+				errorProvider6.SetError(this.textBox6, "Please Enter Class");
+			}
+			else
+			{
+				errorProvider6.Clear();
+			}
+		}
+
+		private void textBox7_Leave(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox7.Text))
+			{
+				textBox7.Focus();
+				errorProvider7.SetError(this.textBox7, "Please Enter Email");
+			}
+			else
+			{
+				errorProvider7.Clear();
+			}
+		}
+
+		private void textBox8_Leave(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox8.Text))
+			{
+				textBox8.Focus();
+				errorProvider8.SetError(this.textBox8, "Please Enter Password");
+			}
+			else
+			{
+				errorProvider8.Clear();
+			}
+		}
+
+		private void textBox9_Leave(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox9.Text))
+			{
+				textBox9.Focus();
+				errorProvider9.SetError(this.textBox9, "Please Enter Confirm Password");
+			}
+			else
+			{
+				errorProvider9.Clear();
+			}
+		}
+
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char ch = e.KeyChar;
+			if(Char.IsDigit(ch) || ch == 8) {
+			e.Handled = false;
+			}
+			else
+			{
+			e.Handled = true;
+			}
+		}
+
+		private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char ch = e.KeyChar;
+			if (Char.IsDigit(ch) || ch == 8)
+			{
+				e.Handled = false;
+			}
+			else
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char ch = e.KeyChar;
+			if(Char.IsLetter(ch) || ch == 8 || ch==32)
+			{
+				e.Handled = false;
+			}
+			else
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char ch = e.KeyChar;
+			if (Char.IsLetter(ch) || ch == 8 || ch == 32)
+			{
+				e.Handled = false;
+			}
+			else
+			{
+				e.Handled = true;
+			}
+		}
+	}
+}
